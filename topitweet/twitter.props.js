4bbcf704c2eb2c69cfa9edfdbd992866340ea953
vdveer <wouter.van.der.veer@topicus.nl>
2014-04-02 17:19:27 +0200

    gevaarlijke push

---
+++ topitweet/twitter.props.js
@@ -0,0 +1,8 @@
+var TWITTER = {
+	APIKEY = 'wBwd52ZJ8HMvQ2h7SLUkDg',
+	CALLBACK = 'https://topitweet.topicusonderwijs.nl/#',
+	AUTHURL = 'https://api.twitter.com/oauth2/token',
+	REQTOKURL = 'https://api.twitter.com/oauth/request_token',
+	AUTHORIZEURL = 'https://api.twitter.com/oauth/authorize',
+	ACCTOKURL = 'https://api.twitter.com/oauth/access_token'
+};
