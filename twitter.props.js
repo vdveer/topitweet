44662dbff70cd224d8702a83191111547c4a8ed7
Aron <aron.kool@topicus.nl>
2014-04-02 17:32:29 +0200

    tweets ophalen

--- twitter.props.js
+++ twitter.props.js
@@ -1,11 +1,11 @@
 var TWITTER = {
-	CONSKEY = 'wBwd52ZJ8HMvQ2h7SLUkDg',
-	CONSSECRET = 'TbUjK8k51tMwfh1cue5NKpGDfHKhvudjEtrKjUzjyQA',
-	OWNERID = '2423931534',
-	OWNER = 'topiconf1234',
-	CALLBACK = 'https://topitweet.topicusonderwijs.nl/#',
-	AUTHURL = 'https://api.twitter.com/oauth2/token',
-	REQTOKURL = 'https://api.twitter.com/oauth/request_token',
-	AUTHORIZEURL = 'https://api.twitter.com/oauth/authorize',
-	ACCTOKURL = 'https://api.twitter.com/oauth/access_token'
+	CONSKEY: 'wBwd52ZJ8HMvQ2h7SLUkDg',
+	CONSSECRET: 'TbUjK8k51tMwfh1cue5NKpGDfHKhvudjEtrKjUzjyQA',
+	OWNERID: '2423931534',
+	OWNER: 'topiconf1234',
+	CALLBACK: 'https://topitweet.topicusonderwijs.nl/#',
+	AUTHURL: 'https://api.twitter.com/oauth2/token',
+	REQTOKURL: 'https://api.twitter.com/oauth/request_token',
+	AUTHORIZEURL: 'https://api.twitter.com/oauth/authorize',
+	ACCTOKURL: 'https://api.twitter.com/oauth/access_token'
 };
